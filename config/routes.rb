d028722057f9ed79a3a51c4708178e308594007a
Iryna Stein <isophiestein@gmail.com>
2021-08-25 19:53:24 -0600

    added routes to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :pets
+  resources :pets, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+  post 'signup', to: "users#create"
+  get 'me', to: "users#show"
+
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: "seesions#destroy"
 end
