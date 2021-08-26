62e1ab1f7d64e7aac49bf7a69148c5c541be73f9
Iryna Stein <isophiestein@gmail.com>
2021-08-25 19:48:31 -0600

    created and migrated all models

---
+++ app/models/age_stage.rb
@@ -0,0 +1,2 @@
+class AgeStage < ApplicationRecord
+end
---
+++ app/models/pet.rb
@@ -0,0 +1,12 @@
+class Pet < ApplicationRecord
+    belongs_to :user
+    belongs_to :breed
+    belongs_to :activity
+    belongs_to :food
+    
+    validates :name, presence: true, uniqueness: true
+    validates :avatar, presence: true
+    validates :food, inclusion: { in: %w(strawberry avocado charcoal icecream)}
+    validates :activity, inclusion: { in: %w(coding ball hiking swimming)}
+
+end
