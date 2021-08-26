74636b0b4b67bed0d206f91f010dd3266f7d1025
Iryna Stein <isophiestein@gmail.com>
2021-08-26 13:41:50 -0600

    added middleware for cross origin

--- config/application.rb
+++ config/application.rb
@@ -39,5 +39,12 @@ module PetsBackend
     config.action_dispatch.cookies_same_site_protection = :strict
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :pets, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  post 'signup', to: "users#create"
-  get 'me', to: "users#show"
+  post '/signup', to: 'users#create'
+  get '/me', to: 'users#show'
 
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: "seesions#destroy"
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: "seesions#destroy"
 end
